9672ebd75bcd6b3377cf5c7d99eb557cd45346e6
Ben Simpson <b.albertsimpson@gmail.com>
2019-10-08 11:55:08 -0700

    updated content

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 client = MongoClient()
 db = client.Contractor
@@ -9,8 +10,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def cinema_index():
-    """Show all playlists."""
-    return render_template('cinema_index.html', cinemas=cinemas.find())
+    """Show all movies"""
+    return render_template('cinema_index.html', movies=movies.find())
+
+@app.route('/cinema/cart')
+def cinema_cart():
+    """Added to the cart"""
+    return render_template('cinema_cart.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
